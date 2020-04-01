7778900c398ff1d59548c8796208b0a56f5e3701
Andres Cruz <andres@kanto.studio>
2020-03-31 21:16:07 -0600

    Script para correr con soluciones.txt funciona, sols tiene soluciones

--- run.py
+++ run.py
@@ -10,13 +10,23 @@ if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
+        index = 0
+        if len(sys.argv) > 2:
+            index = int(sys.argv[2].strip())
         with open(file_location, 'r') as args_file:
             args = args_file.readlines()
-        print(args)
-        line = args[0].strip()
-        arg_arr = line.split(' ')
-        print(arg_arr)
-        print(run(['python3', 'solver.py'] + arg_arr))
-        # for line in args:
-        #     arg_arr = line.split(' ')
-        #     print(run(['python3'] + arg_arr))
+        # print(args)
+        # line = args[0].strip()
+        # arg_arr = line.split(' ')
+        # print(arg_arr)
+        # print(run(['python3', 'solver.py'] + arg_arr))
+        for i in range(index, len(args)):
+            line = args[i]
+            arg_arr = line.strip().split(' ')
+            res = run(['python3', 'solver.py'] + arg_arr)
+            write_file = arg_arr[0].split('/')[2]
+            write_file = './sols/' + write_file
+            print(write_file)
+            f = open(write_file, "w")
+            f.write(res)
+            f.close()
